7f033159ba88bc052dbce076156d16486272ef22
nicolezca <tatilezcano03@gmail.com>
2023-10-05 15:52:36 -0300

    manejo del DOM actualizado

--- pages/js/salas.js
+++ pages/js/salas.js
@@ -1,6 +1,22 @@
 
 //filtrado por nombre de sala y estado
 document.addEventListener("DOMContentLoaded", function () {
+
+    const btnform = document.getElementById("mostrarFormulario");
+    const formulario = document.getElementById("formDoc");
+
+    // Establece un valor predeterminado para el estilo transform
+    formulario.style.transform = "translateX(-100%)";
+
+    btnform.addEventListener("click", function () {
+        if (formulario.style.transform === "translateX(-100%)") {
+            formulario.style.transform = "translateX(0)";
+        } else {
+            formulario.style.transform = "translateX(-100%)";
+        }
+    });
+
+
     const filternameInput = document.getElementById("filter_name");
     const filterEstadoInput = document.getElementById("filter_estado");
     const aplicarFiltroButton = document.getElementById("aplicarFiltro");
@@ -9,7 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
     aplicarFiltroButton.addEventListener("click", function () {
         const estado = filterEstadoInput.value.toLowerCase();
         const nombre = filternameInput.value.toLowerCase();
-        filtrarTabla(estado,nombre);
+        filtrarTabla(estado, nombre);
     });
 
     function filtrarTabla(estado, nombre) {
@@ -19,7 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
             const columnas = fila.getElementsByTagName("td");
             const nombreColumna = columnas[1].textContent.toLowerCase(); // Columna del nombre
             const estadoColumna = columnas[6].textContent.toLowerCase(); // Columna de estado
-            if ((estadoColumna.includes(estado) || estado === "") && (nombreColumna.includes(nombre) || nombre ==="") ) {
+            if ((estadoColumna.includes(estado) || estado === "") && (nombreColumna.includes(nombre) || nombre === "")) {
                 fila.style.display = "table-row";
             } else {
                 fila.style.display = "none";
