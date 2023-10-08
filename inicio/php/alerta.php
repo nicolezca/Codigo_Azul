881da43412d3417176e31a93edcf04c4b2a41bac
nicolezca <tatilezcano03@gmail.com>
2023-10-08 07:04:14 -0300

    verifiacion de capacidad en salas

--- inicio/php/alerta.php
+++ inicio/php/alerta.php
@@ -9,33 +9,59 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["sala"]) && isset($_POS
     $fechaInicio = $_POST['fechaInicio'];
     $prioridad = $_POST['prioridad']; // tipo de prioridad (normal o emergencia)
 
-    // Primero, se inserta en la tabla llamado
-    $sql = "INSERT INTO llamado (idSala, idPaciente, fechaHoraInicio, fechaHoraFin, prioridadLlamada) 
-            VALUES ('$sala', '$paciente', '$fechaInicio', ' ', '$prioridad')";
+    // Verificar la capacidad de la sala
+    $sqlCapacidad = "SELECT capacidadMaxima, ocupacionActual FROM sala WHERE id = $sala";
+    $resultCapacidad = $conn->query($sqlCapacidad);
 
-    if ($conn->query($sql) === TRUE) {
-        // Obten el idLlamado generado automáticamente
-        $idLlamado = $conn->insert_id;
+    if ($resultCapacidad->num_rows > 0) {
+        $salaInfo = $resultCapacidad->fetch_assoc();
+        $capacidadMaxima = $salaInfo['capacidadMaxima'];
+        $ocupacionActual = $salaInfo['ocupacionActual'];
 
-        // Ahora, inserta en la tabla sala_personal utilizando el idLlamado
-        $sql2 = "INSERT INTO llamado_personal (idLlamado, idPersonal) VALUES ('$idLlamado', '$personal')";
+        if ($ocupacionActual < $capacidadMaxima) {
+            // La sala tiene capacidad disponible, puedes asignar al paciente
 
-        if ($conn->query($sql2) === TRUE) {
-            // Actualizar el estado del paciente a "baja"
-            $sql3 = "UPDATE paciente SET estado = 'baja' WHERE id = '$paciente'";
+            // Primero, se inserta en la tabla llamado
+            $sql = "INSERT INTO llamado (idSala, idPaciente, fechaHoraInicio, fechaHoraFin, prioridadLlamada) 
+                    VALUES ('$sala', '$paciente', '$fechaInicio', ' ', '$prioridad')";
 
-            if ($conn->query($sql3) === TRUE) {
-                header("location:../../pages/pacientes/atendidos.php");
+            if ($conn->query($sql) === TRUE) {
+                // Obten el idLlamado generado automáticamente
+                $idLlamado = $conn->insert_id;
+
+                // Actualiza la ocupación actual de la sala
+                $ocupacionActual++;
+
+                // Actualiza la sala en la base de datos
+                $sqlActualizarSala = "UPDATE sala SET ocupacionActual = $ocupacionActual WHERE id = $sala";
+                $conn->query($sqlActualizarSala);
+
+                // Ahora, inserta en la tabla sala_personal utilizando el idLlamado
+                $sql2 = "INSERT INTO llamado_personal (idLlamado, idPersonal) VALUES ('$idLlamado', '$personal')";
+
+                if ($conn->query($sql2) === TRUE) {
+                    // Actualizar el estado del paciente a "baja"
+                    $sql3 = "UPDATE paciente SET estado = 'baja' WHERE id = '$paciente'";
+
+                    if ($conn->query($sql3) === TRUE) {
+                        header("location:../../pages/pacientes/atendidos.php");
+                    } else {
+                        echo "Error al actualizar el estado del paciente: " . $conn->error;
+                    }
+                } else {
+                    echo "Error al asignar la sala: " . $conn->error;
+                }
             } else {
-                echo "Error al actualizar el estado del paciente: " . $conn->error;
+                echo "Error al insertar el llamado: " . $conn->error;
             }
         } else {
-            echo "Error al asignar la sala: " . $conn->error;
+            echo "La sala está llena, no se puede asignar al paciente.";
         }
     } else {
-        echo "Error al insertar el llamado: " . $conn->error;
+        echo "Error al obtener la información de la sala: " . $conn->error;
     }
 }
 
 $conn->close();
+
 ?>
