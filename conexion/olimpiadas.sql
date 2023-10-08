e5a1fac9972f92f8b019b08d131fed2a67da1a89
nicolezca <tatilezcano03@gmail.com>
2023-10-08 04:54:12 -0300

    actualizacion de archivo SQL

--- conexion/olimpiadas.sql
+++ conexion/olimpiadas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 27-09-2023 a las 12:03:02
+-- Tiempo de generación: 08-10-2023 a las 09:53:19
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -29,13 +29,24 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `llamado` (
   `id` int(11) NOT NULL,
-  `estado` enum('atendido','no_atendido','finalizado') NOT NULL,
   `fechaHoraInicio` datetime NOT NULL,
   `fechaHoraFin` datetime DEFAULT NULL,
   `idPaciente` int(11) DEFAULT NULL,
   `idSala` int(11) DEFAULT NULL,
   `prioridadLlamada` enum('normal','emergencia') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Volcado de datos para la tabla `llamado`
+--
+
+INSERT INTO `llamado` (`id`, `fechaHoraInicio`, `fechaHoraFin`, `idPaciente`, `idSala`, `prioridadLlamada`) VALUES
+(1, '2023-10-03 20:55:00', '0000-00-00 00:00:00', 4, 3, 'emergencia'),
+(2, '2023-10-03 23:36:00', '0000-00-00 00:00:00', 3, 1, 'emergencia'),
+(3, '2023-10-08 00:52:00', '0000-00-00 00:00:00', 3, 2, 'emergencia'),
+(4, '2023-10-08 00:58:00', '0000-00-00 00:00:00', 3, 3, 'emergencia'),
+(5, '2023-10-08 00:58:00', '0000-00-00 00:00:00', 3, 3, 'emergencia'),
+(6, '2023-10-08 00:59:00', '0000-00-00 00:00:00', 2, 1, 'normal');
 
 -- --------------------------------------------------------
 
@@ -48,6 +59,17 @@ CREATE TABLE `llamado_personal` (
   `idLlamado` int(11) NOT NULL,
   `idPersonal` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Volcado de datos para la tabla `llamado_personal`
+--
+
+INSERT INTO `llamado_personal` (`id`, `idLlamado`, `idPersonal`) VALUES
+(1, 1, 3),
+(3, 3, 2),
+(4, 4, 3),
+(5, 5, 3),
+(6, 6, 5);
 
 -- --------------------------------------------------------
 
@@ -62,20 +84,23 @@ CREATE TABLE `paciente` (
   `dni` varchar(10) NOT NULL,
   `telefono` varchar(20) DEFAULT NULL,
   `obraSocial` varchar(255) DEFAULT NULL,
-  `historiaClinica` text DEFAULT NULL
+  `historiaClinica` text DEFAULT NULL,
+  `estado` enum('alta','baja','espera','') NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 --
 -- Volcado de datos para la tabla `paciente`
 --
 
-INSERT INTO `paciente` (`id`, `nombre`, `apellido`, `dni`, `telefono`, `obraSocial`, `historiaClinica`) VALUES
-(1, 'Agustin', 'Lezcano', '46690052', '2241562603', 'IOMA', 'entro por una farinjitis aguda'),
-(2, 'Juan', 'Pérez', '12345678', '555-1234', 'PAMI', 'El paciente se sometió a una cirugía de apendicectomía la semana pasada'),
-(3, 'María', 'González', '98765432', '555-5678', 'no tiene', 'El paciente sufre de migrañas frecuentes'),
-(4, 'Ana', 'Rodriguez', '12345678', '1162114065', 'Swiss Medical', 'La paciente informa de dolor en la espalda baja después de levantar objetos pesados'),
-(5, 'María', 'Garcia', '34567890', '2241555-9012', 'O.S.P.S.A', 'La paciente se presenta con dolor abdominal agudo'),
-(6, 'María', 'Garcia', '34567890', '2241555-9012', 'O.S.P.S.A', 'La paciente se presenta con dolor abdominal agudo');
+INSERT INTO `paciente` (`id`, `nombre`, `apellido`, `dni`, `telefono`, `obraSocial`, `historiaClinica`, `estado`) VALUES
+(1, 'Agustin', 'Lezcano', '46690052', '2241562603', 'IOMA', 'entro por una farinjitis aguda', 'alta'),
+(2, 'Juan', 'Pérez', '12345678', '555-1234', 'PAMI', 'El paciente se sometió a una cirugía de apendicectomía la semana pasada', 'baja'),
+(3, 'María', 'González', '98765432', '555-5678', 'no tiene', 'El paciente sufre de migrañas frecuentes', 'baja'),
+(4, 'Ana', 'Rodriguez', '12345678', '1162114065', 'Swiss Medical', 'La paciente informa de dolor en la espalda baja después de levantar objetos pesados', 'espera'),
+(5, 'María', 'Garcia', '34567890', '2241555-9012', 'O.S.P.S.A', 'La paciente se presenta con dolor abdominal agudo', 'alta'),
+(6, 'María', 'Garcia', '34567890', '2241555-9012', 'O.S.P.S.A', 'La paciente se presenta con dolor abdominal agudo', 'baja'),
+(7, 'Joquin', 'Chavez', '48287298', '224124321', 'IOMA', 'El paciente se sometió a una cirugía de apendicectomía la semana pasada', 'baja'),
+(8, 'teta lover', 'rouco', '44446667', '2241666666', 'si ', 'el paciente entropor un dolor fuerte en la cabeza', 'espera');
 
 -- --------------------------------------------------------
 
@@ -103,7 +128,9 @@ INSERT INTO `personal` (`id`, `nombre`, `apellido`, `cargo`, `matricula`, `tipo`
 (4, 'Carlos', 'López', 'Ginecólogo', '78901', 'medico'),
 (5, 'Ana', 'Martínez', 'Cirujano', '34567', 'medico'),
 (6, 'Javier', 'Sánchez', 'Dermatólogo', '89012', 'medico'),
-(7, 'Susana', 'Ramírez', 'Oftalmólogo', '45678', 'medico');
+(7, 'Susana', 'Ramírez', 'Oftalmólogo', '45678', 'medico'),
+(8, 'Agustin', 'Lezcano', 'Enfermero General', '08062005', 'enfermero'),
+(9, 'Romina', 'Chavez', 'Enfermera de Urgencias', '874320', 'enfermero');
 
 -- --------------------------------------------------------
 
@@ -120,6 +147,15 @@ CREATE TABLE `sala` (
   `capacidadMaxima` int(11) NOT NULL,
   `ocupacionActual` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+--
+-- Volcado de datos para la tabla `sala`
+--
+
+INSERT INTO `sala` (`id`, `nombre`, `piso`, `disponible`, `tipo`, `capacidadMaxima`, `ocupacionActual`) VALUES
+(1, 'Quirofano', 2, 1, 'sala', 12, 11),
+(2, 'dentista', 2, 1, 'sala', 12, 3),
+(3, 'Pediatria', 1, 1, 'sala', 5, 1);
 
 -- --------------------------------------------------------
 
@@ -239,31 +275,31 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de la tabla `llamado`
 --
 ALTER TABLE `llamado`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `llamado_personal`
 --
 ALTER TABLE `llamado_personal`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `paciente`
 --
 ALTER TABLE `paciente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `personal`
 --
 ALTER TABLE `personal`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de la tabla `sala`
 --
 ALTER TABLE `sala`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `sala_paciente`
