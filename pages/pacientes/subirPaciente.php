58033ee1ac3432098ae324de003f12a660d35d28
nicolezca <tatilezcano03@gmail.com>
2023-10-26 04:47:23 -0300

    se modifico el isset de estado para una carga automatica de estado

--- pages/pacientes/subirPaciente.php
+++ pages/pacientes/subirPaciente.php
@@ -2,25 +2,24 @@
 
 include('../../conexion/conexion.php');
 
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nombre"]) && isset($_POST["apellido"]) && isset($_POST["dni"]) && isset($_POST["telefono"])  && isset($_POST["social"])  && isset($_POST["historial"])  && isset($_POST["estado"])) {
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nombre"]) && isset($_POST["apellido"]) && isset($_POST["dni"]) && isset($_POST["telefono"])  && isset($_POST["social"])  && isset($_POST["historial"]) ) {
     $nombre = $_POST["nombre"];
     $apellido = $_POST["apellido"];
     $dni = $_POST["dni"];
     $telefono = $_POST["telefono"];
     $social = $_POST["social"];
     $historial = $_POST["historial"];
-    $estado = $_POST["estado"];
 
 
 // Insertar el nuevo paciente en la tabla paciente
-$sql = "INSERT INTO paciente (nombre, apellido, dni, telefono, obraSocial, estado ) VALUES ('$nombre', '$apellido', '$dni', '$telefono','$social','$estado')";
+$sql = "INSERT INTO paciente (nombre, apellido, dni, telefono, obraSocial, estado ) VALUES ('$nombre', '$apellido', '$dni', '$telefono','$social','espera')";
 
 if ($conn->query($sql) === TRUE) {
     // Obtener el idPaciente generado automáticamente
     $idPaciente = $conn->insert_id;
     
     // Insertar el historial clínico en la tabla historia_clinica
-    $sqlHistorial = "INSERT INTO historia_clinica (idPaciente, contenido) VALUES ('$idPaciente', '$historial')";
+    $sqlHistorial = "INSERT INTO historia_clinica (idPaciente, contenido)    VALUES ('$idPaciente', '$historial')";
     
     if ($conn->query($sqlHistorial) === TRUE) {
         header("Location: pacientes.php");
