d9011addfc442b85f66ac7c0c03b3cdcd7dc94a1
nicolezca <tatilezcano03@gmail.com>
2023-10-28 01:22:01 -0300

    se modifico las consultas y la extraccion de datos del formulario

--- pages/pacientes/altaPacientes.php
+++ pages/pacientes/altaPacientes.php
@@ -5,34 +5,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["paciente"]) && isset($
     $idPaciente = $_POST["paciente"];
     $fechafin = $_POST["fechafin"];
 
-    // Consulta SQL para actualizar la fechaHoraFin del último registro de llamado para el paciente seleccionado
-    $sqlActualizarLlamado = "UPDATE llamado SET fechaHoraFin = '$fechafin' WHERE idPaciente = $idPaciente  AND fechaHoraFin ='0000-00-00 00:00:00' ORDER BY id DESC LIMIT 1";
-
-    // Consulta SQL para actualizar el estado del paciente a "alta"
-    $sqlActualizarPaciente = "UPDATE paciente SET estado = 'alta' WHERE id = $idPaciente";
-
-    // Obtener el id de la sala asociada al paciente
-    $sqlObtenerSala = "SELECT idSala FROM llamado WHERE idPaciente = $idPaciente AND fechaHoraFin ='0000-00-00 00:00:00' ORDER BY id DESC LIMIT 1";
+    // Obtener el id de la sala asociada desde la tabla sala_paciente
+    $sqlObtenerSala = "SELECT idSala FROM sala_paciente WHERE idPaciente = $idPaciente AND fechaHoraEgreso = '0000-00-00 00:00:00' ORDER BY id DESC LIMIT 1";
     $resultSala = $conn->query($sqlObtenerSala);
 
     if ($resultSala->num_rows > 0) {
         $rowSala = $resultSala->fetch_assoc();
         $idSala = $rowSala['idSala'];
 
-        // Consulta SQL para reducir la ocupación actual de la sala en 1
+        // Insertar la fecha de egreso en la tabla sala_paciente
+        $sqlActualizarSalaPaciente = "UPDATE sala_paciente SET fechaHoraEgreso = '$fechafin' WHERE idPaciente = $idPaciente AND fechaHoraEgreso = '0000-00-00 00:00:00' ORDER BY id DESC LIMIT 1";
+        $conn->query($sqlActualizarSalaPaciente);
+
+        // Reducir la ocupación actual de la sala en 1
         $sqlReducirOcupacion = "UPDATE sala SET ocupacionActual = ocupacionActual - 1 WHERE id = $idSala";
         $conn->query($sqlReducirOcupacion);
     }
+
+    // Actualizar el estado del paciente a "fuera"
+    $sqlActualizarPaciente = "UPDATE paciente SET estado = 'fuera' WHERE id = $idPaciente";
 
     // Ejecutar las consultas en una transacción
     $conn->begin_transaction();
 
     try {
-        $conn->query($sqlActualizarLlamado);
         $conn->query($sqlActualizarPaciente);
         $conn->commit();
 
-        header("Location: pacientes.php");
+        header("Location: ../salas/salas.php");
         exit();
     } catch (Exception $e) {
         // En caso de error, revertir la transacción
@@ -43,5 +43,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["paciente"]) && isset($
 } else {
     echo "Por favor, complete todos los campos del formulario.";
 }
-
 ?>
