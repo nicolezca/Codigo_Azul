16de7fd6491b27ff98e74240b314903f889a9f0b
nicolezca <tatilezcano03@gmail.com>
2023-10-30 01:58:12 -0300

    se agrego biblioteca de vendor para envio de SMS

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'a15a59f8e023bdff0a841d2e05bb5e92da9ae846',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'a15a59f8e023bdff0a841d2e05bb5e92da9ae846',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twilio/sdk' => array(
+            'pretty_version' => '7.12.0',
+            'version' => '7.12.0.0',
+            'reference' => 'f281d7b793f02377ab0f38e7166e16820acc3eae',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twilio/sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
