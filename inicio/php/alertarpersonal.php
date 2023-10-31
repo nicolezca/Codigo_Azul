597cba483a07912983727774267dd3a35cdb8754
nicolezca <tatilezcano03@gmail.com>
2023-10-30 21:21:40 -0300

    se creo un array para mandar a los destinatarios el mensaje

--- inicio/php/alertarpersonal.php
+++ inicio/php/alertarpersonal.php
@@ -3,6 +3,15 @@ session_start();
 include('../../conexion/conexion.php');
 
     $nombreSala = $_SESSION['nombre'];
+    $telEnfer = $_SESSION['telefono_enfermero'];
+    $telDoc = $_SESSION['telefono_doctor'];
+
+    // Lista de personal y sus números de teléfono
+$personal = array(
+    array('telefono' => $telDoc),
+    array('telefono' => $telEnfer),
+    // Agrega más personal aquí si es necesario
+);
 
 require '../../vendor/autoload.php';
 
@@ -14,29 +23,26 @@ $fromNumber = '+12255353683';
 // Crea un cliente Twilio
 $client = new Twilio\Rest\Client($sid, $token);
 
-// Número de teléfono de destino y mensaje
-$destinatario =  '+542241562603'; 
-$mensaje = 'ha sido solicitado reportese a la sala: ' . $nombreSala ;
+foreach ($personal as $individuo) {
+    $destinatario = $individuo['telefono'];
+    $mensaje = 'Ha sido solicitado reportarse a la sala: ' . $nombreSala;
 
-// Envía el SMS
-try {
-    $message = $client->messages->create(
-        $destinatario,
-        array(
-            'from' => $fromNumber,
-            'body' => $mensaje
-        )
-    );
-    header(
-        "Location:../inicio.php"
-    );
-    //header(
-      //  "Location: ../../pages/llamado/atencion.php");
-    //exit();
-} catch (Exception $e) {
-    // Captura y muestra cualquier error que ocurra
-    echo 'Error al enviar el mensaje: ' . $e->getMessage();
+    try {
+        $message = $client->messages->create(
+            $destinatario,
+            array(
+                'from' => $fromNumber,
+                'body' => $mensaje
+            )
+        );
+
+        // Puedes registrar que se envió un mensaje a $nombre o realizar otras acciones aquí
+    } catch (Exception $e) {
+        // Captura y muestra cualquier error que ocurra
+        echo 'Error al enviar el mensaje a ' . $nombre . ': ' . $e->getMessage();
+    }
 }
-
-
+// Redirige a donde sea necesario después de enviar los mensajes
+header("Location: ../inicio.php");
+//Asegúrate de reemplazar $telDoc y $telEnfer con los números de teléfono reales de tu personal y ajustar el bucle según tus necesidades. Esto enviará mensajes a cada destinatario en la lista de personal.
 ?>
