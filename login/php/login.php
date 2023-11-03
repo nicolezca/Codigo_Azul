09e083c42326b2295c255d3e28a464ea1fd4eb5d
nicolezca <tatilezcano03@gmail.com>
2023-11-03 03:14:20 -0300

    se refactorizo el codigo del Login/Register

--- login/php/login.php
+++ login/php/login.php
@@ -1,33 +1,39 @@
 <?php
-    include('../../conexion/conexion.php');
+session_start();
 
-    session_start();
+function validarInicioSesion($conn, $nombre, $clave) {
+    $sql = "SELECT * FROM usuario WHERE nombre = ? AND contrasena = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("ss", $nombre, $clave);
+    $stmt->execute();
+    $result = $stmt->get_result();
 
-// Procesar inicio de sesión
-if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nombre"]) && isset($_POST["clave"])) {
-    $nombre = $_POST["nombre"];
-    $clave = $_POST["clave"];
-    
-    // Validar los campos de inicio de sesión
-    if (empty($nombre) || empty($clave)) {
-        echo "Por favor, complete todos los campos.";
+    if ($result->num_rows == 1) {
+        $_SESSION["nombre"] = $nombre;
+        $_SESSION["clave"] = $clave;
+        header("Location: ../../inicio/inicio.php");
+        exit();
     } else {
-        // Realizar la validación de las credenciales en la base de datos
-        $sql = "SELECT * FROM usuario WHERE nombre = '$nombre' AND contrasena = '$clave'";
-        $result = $conn->query($sql);
-        
-        if ($result->num_rows == 1) {
-            $_SESSION["nombre"] = $nombre;
-            $_SESSION["clave"] = $clave;
-            header("Location: ../../inicio/inicio.php");
-            exit();
-        } else {
-            echo "Credenciales inválidas";
-            echo '<a href="../formulario.html">volver a intentar</a>';
-        }
+        echo "Credenciales inválidas";
+        echo '<a href="../formulario.html">Volver a intentar</a>';
     }
 }
 
-// Cerrar la conexión a la base de datos
-$conn->close();
+if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nombre"]) && isset($_POST["clave"])) {
+    $nombre = $_POST["nombre"];
+    $clave = $_POST["clave"];
+
+    if (empty($nombre) || empty($clave)) {
+        echo "Por favor, complete todos los campos.";
+    } else {
+        include('../../conexion/conexion.php');
+
+        $nombre = mysqli_real_escape_string($conn, $nombre);
+        $clave = mysqli_real_escape_string($conn, $clave);
+
+        validarInicioSesion($conn, $nombre, $clave);
+
+        $conn->close();
+    }
+}
 ?>
--- login/php/registro.php
+++ login/php/registro.php
@@ -1,45 +1,50 @@
 <?php
+session_start();
 
-include('../../conexion/conexion.php');
-// Procesar registro
+function registrarUsuario($conn, $nombre, $clave, $tipo) {
+    $sql = "INSERT INTO usuario (nombre, contrasena, tipo) VALUES (?, ?, ?)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("sss", $nombre, $clave, $tipo);
+
+    if ($stmt->execute()) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["nombreingreso"]) && isset($_POST["claveIngreso"]) && (isset($_POST["admin"]) || isset($_POST["generico"]))) {
     $nombre = $_POST["nombreingreso"];
     $clave = $_POST["claveIngreso"];
-    $tipo = "";
+    $tipo = isset($_POST["admin"]) ? "admin" : "generico";
 
-    if (isset($_POST["admin"])) {
-        $tipo = "admin";
-    } elseif (isset($_POST["generico"])) {
-        $tipo = "generico";
-    }
-
-    // Validar los campos de registro
     if (empty($nombre) || empty($clave) || empty($tipo)) {
         echo "Por favor, complete todos los campos.";
     } else {
-        // Verificar si el nombre ya está registrado en la base de datos
-        $sql = "SELECT * FROM usuario WHERE nombre = '$nombre'";
-        $result = $conn->query($sql);
+        include('../../conexion/conexion.php');
+
+        $nombre = mysqli_real_escape_string($conn, $nombre);
+
+        $sql = "SELECT * FROM usuario WHERE nombre = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $nombre);
+        $stmt->execute();
+        $result = $stmt->get_result();
 
         if ($result->num_rows > 0) {
             echo "El nombre de usuario ya está registrado.";
         } else {
-            // Insertar el nuevo usuario en la base de datos
-            $sql = "INSERT INTO usuario (nombre, contrasena, tipo) VALUES ('$nombre', '$clave', '$tipo')";
-
-            if ($conn->query($sql) === TRUE) {
+            if (registrarUsuario($conn, $nombre, $clave, $tipo)) {
                 echo "Registro exitoso";
-                // Redireccionar a la página de inicio de sesión o a otra página de tu elección
                 header("Location: ../../inicio/inicio.php");
                 exit();
             } else {
-                echo "Error en el registro: " . $conn->error;
+                echo "Error en el registro.";
                 echo '<a href="login.php">Volver a intentar</a>';
             }
         }
+
+        $conn->close();
     }
 }
-
-// Cerrar la conexión a la base de datos
-$conn->close();
 ?>
