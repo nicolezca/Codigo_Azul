37524f2c190e3ca19549cf1b0a4bad5924fdb7b5
nicolezca <tatilezcano03@gmail.com>
2023-11-03 04:34:03 -0300

    Se mejoro la seguridad de la conexion

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,14 +1,8 @@
 <?php
-$hostname = 'localhost';
-$database = 'olimpiadas';
-$username = 'root'; 
-$password = '';
+include('config.php');
 
-// Crear una conexión
-$conn = new mysqli($hostname, $username, $password, $database);
+$conn = new mysqli($dbConfig['hostname'], $dbConfig['username'], $dbConfig['password'], $dbConfig['database']);
 
-// Verificar la conexión
 if ($conn->connect_error) {
     die("Error en la conexión a la base de datos: " . $conn->connect_error);
 }
-?>
