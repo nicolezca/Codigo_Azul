648e0c734e675665d858a26d54e01f2bb010bb66
nicolezca <tatilezcano03@gmail.com>
2023-11-03 04:10:53 -0300

    Refactorizacion a Histotial nuevo

--- pages/pacientes/insertarHistorial.php
+++ pages/pacientes/insertarHistorial.php
@@ -1,35 +1,45 @@
 <?php
 include('../../conexion/conexion.php');
-// Verifica si se ha enviado el formulario
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['paciente']) && isset($_POST['historial'])) {
+        $idPaciente = $_POST['paciente'];
+        $historiaClinica = $_POST['historial'];
 
-    // Obtiene los datos del formulario
-    $idPaciente = $_POST['paciente'];
-    $historiaClinica = $_POST['historial'];
+        // Escapa las variables para prevenir inyecciones SQL
+        $idPaciente = mysqli_real_escape_string($conn, $idPaciente);
+        $historiaClinica = mysqli_real_escape_string($conn, $historiaClinica);
 
-    // Realiza la inserción en la tabla de historia_clínica
-    $sqlInsertHistoria = "INSERT INTO historia_clinica (idPaciente, contenido) VALUES ($idPaciente, '$historiaClinica')";
+        // Crea una sentencia preparada para insertar la historia clínica
+        $sqlInsertHistoria = "INSERT INTO historia_clinica (idPaciente, contenido) VALUES (?, ?)";
+        $stmtInsertHistoria = $conn->prepare($sqlInsertHistoria);
+        $stmtInsertHistoria->bind_param("is", $idPaciente, $historiaClinica);
 
-    // La historia clínica se insertó con éxito
-    if ($conn->query($sqlInsertHistoria) === true) {
+        if ($stmtInsertHistoria->execute()) {
+            
+            // Actualiza el estado del paciente a "espera"
+            $sqlUpdatePaciente = "UPDATE paciente SET estado = 'espera' WHERE id = ?";
+            $stmtUpdatePaciente = $conn->prepare($sqlUpdatePaciente);
+            $stmtUpdatePaciente->bind_param("i", $idPaciente);
 
-        // Actualiza el estado del paciente a "espera"
-        $sqlUpdatePaciente = "UPDATE paciente SET estado = 'espera' WHERE id = $idPaciente";
-
-        if ($conn->query($sqlUpdatePaciente) === true) {
-            header(
-                "Location:pacientes.php"
-            );
-            exit();
+            if ($stmtUpdatePaciente->execute()) {
+                header("Location: pacientes.php");
+                exit();
+            } else {
+                echo "Error al actualizar el estado del paciente: " . $conn->error;
+            }
         } else {
-            echo "Error al actualizar el estado del paciente: " . $conn->error;
+            echo "Error al insertar la historia clínica: " . $conn->error;
         }
-    } else {
-        // Ocurrió un error al insertar la historia clínica
-        echo "Error al insertar la historia clínica: " . $conn->error;
-    }
 
-    // Cierra la conexión a la base de datos
-    $conn->close();
+        // Cierra las sentencias preparadas
+        $stmtInsertHistoria->close();
+        $stmtUpdatePaciente->close();
+    } else {
+        echo "Faltan datos en el formulario.";
+    }
 }
+
+// Cierra la conexión a la base de datos
+$conn->close();
 ?>
